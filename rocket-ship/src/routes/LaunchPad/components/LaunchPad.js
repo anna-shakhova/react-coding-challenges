83a1775e85568fdbf19ad9b7314fa6ea63e73375
anna-shakhova <vasilyevaanna@gmail.com>
2021-03-22 11:16:26 +0300

    'rocket'

--- rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
+++ rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import { ClassRocket, FunctionalRocket } from './Rocket';
 import '../styles/_launchpad.scss';
 
 export default function LaunchPad() {
   const [rerenderCount, triggerRerender] = useState(0);
 
-  setTimeout(() => { triggerRerender(rerenderCount + 1); }, 500);
+  useEffect(() => {
+    setTimeout(() => { triggerRerender(rerenderCount + 1); }, 500);
+  }, [rerenderCount]);
 
   return (
     <div className="launchpad">
-      <ClassRocket />
+      <Switch>
+        <Route exact path="/class">
+          <ClassRocket />
+        </Route>
+        <Route exact path="/func">
+          <FunctionalRocket />
+        </Route>
+        <Route path="/">
+          <Redirect to="/class" />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- rocket-ship/src/routes/index.js
+++ rocket-ship/src/routes/index.js
@@ -1,5 +1,18 @@
+import './styles/_routes.scss';
+import React from 'react';
+import {BrowserRouter, Link} from 'react-router-dom';
 import LaunchPad from './LaunchPad';
 
 // Use something like react-router-dom to manage multiple pages/routes
 
-export default LaunchPad;
+const Routes = () => (
+  <BrowserRouter>
+    <div className="links">
+      <Link to="/class">Class Component</Link>
+      <Link to="/func">Functional Component</Link>
+    </div>
+    <LaunchPad/>
+  </BrowserRouter>
+);
+
+export default Routes;
